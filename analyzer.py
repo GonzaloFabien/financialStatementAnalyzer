5bb6e198d372342a69ad20e379f7a7f4c22affe7
laptop <garaoz@unsa.edu.pe>
2026-07-28 12:00:32 -0500

    conexion al documento exitoso

--- analyzer.py
+++ analyzer.py
@@ -1,1 +1,11 @@
-#Inicio del programa:
+#The proyect start:
+#1- The first step is know the Scheme="URL" and also the code of the <XBRLI:identifier>B08361
+#2- The second step is extract and understand the times periods.
+
+import xml.etree.ElementTree as ET
+
+#nos conectamos al archivo:
+tree = ET.parse('casaGrande2024.xml')
+root = tree.getroot()
+
+print(f"etique principal: {root.tag}")
