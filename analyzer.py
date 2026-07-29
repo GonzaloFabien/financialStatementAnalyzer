87c0e6b085252ea8a9a8293c280b00a9fb999b37
laptop <garaoz@unsa.edu.pe>
2026-07-29 01:28:26 -0500

    test de tags

--- analyzer.py
+++ analyzer.py
@@ -8,4 +8,15 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('casaGrande2024.xml')
 root = tree.getroot()
 
-print(f"etique principal: {root.tag}")
+#Creamos el namespaces para que python entienda los tag de XBRLI: IFRS
+namespaces = {'xbrli' : 'http://www.xbrl.org/2003/instace',
+              'ifrs-full' : 'http://ifrs.org'}
+
+#para buscar nuestro tabulador:
+primer_elemento_ifrs = root.find('.//ifrs-full:*', namespaces)
+
+#¿Se encontró un elemento?
+if primer_elemento_ifrs is not None:
+    print(f"Correctos NameSpaces, primera tag es: {primer_elemento_ifrs.tag}")
+else:
+    print("No se encontró las etiquetas del URL")
