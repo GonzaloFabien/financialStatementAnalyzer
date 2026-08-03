201f014116ca80530f72afe5ec062c5d93d23f91
Fabien A <garaoz@unsa.edu.pe>
2026-08-02 21:01:06 -0500

    Creamos un buscador de ID en funcion_periodo.py

--- funcion_periodo.py
+++ funcion_periodo.py
@@ -7,7 +7,15 @@ tree = ET.parse(empresa_a_analizar)
 root = tree.getroot()
 
 def encontrar_id_año(año_buscado):
-    print(f"Buscando el ID de {año_buscado}")
+    for elemento in root.iter():
+        nombre_original = elemento.tag
+        nombre_limpio = nombre_original.split('}')[-1] if '}' in nombre_original else nombre_original
+        
+        if nombre_limpio == "context":
+
+            id_contexto = elemento.get('id')
+            print(f"Fecha del ID es = {id_contexto}")
+        
 
 
 
