b9a6349a26e2de910f90577be962db006499ff9b
laptop <garaoz@unsa.edu.pe>
2026-08-28 23:59:29 -0500

    Añadimos una BD JSON NO-SQL para gestionar los datos de los tag xml

--- test.py
+++ test.py
@@ -5,9 +5,10 @@ from analyzer import analizar_empresa
 from analyzer import analizar_solvencia
 from analyzer import extraer_source_data_f1
 from analyzer import calcular_ratios_de_extraer_source_data_f2
+import json
 
 # Aquí el archivo de la empresa a Analizar 
-casa_grande_root = (ET.parse('data_xml\casaGrande2024.xml')).getroot()
+casa_grande_root = (ET.parse('data_xml/casaGrande2024.xml')).getroot()
 cartavio_root = (ET.parse('data_xml/cartavio2024.xml')).getroot()
 
 #Los años se expresan en ids contextRef:
@@ -46,3 +47,10 @@ for ratio in ratios_a_mostrar:
     valor_Cartavio = tabla_horizontal['Cartavio'][ratio]
     print(f"{ratio:<25} | {valor_casaGrande:<13.2f} | {valor_Cartavio:<13.2f}")
 print("-"*40)
+
+#Creación de una base de datos noSQL para manejar los datos en Json:
+
+
+
+with open("data_xml/reporte_analizado.json", "w", encoding="utf-8") as archivo_json:
+    json.dump(tabla_horizontal,archivo_json, indent=4, ensure_ascii=False)
