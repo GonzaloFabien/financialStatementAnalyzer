a4ae73774cb4a2b2f2f12f286fcf71915ca782bb
Fabien <garaoz@unsa.edu.pe>
2026-09-05 23:57:55 -0500

    ver los años disponibles en la Data Base JSON: para Casa Grande

--- app_frontend.py
+++ app_frontend.py
@@ -11,4 +11,14 @@ with open(ruta_json, "r", encoding="utf-8") as archivo:
 
 print("Se cargó correctamente la base de datos")
 
+#3- Accedemos a los datos de la empresa
+datos_empresa = base_datos['Casa Grande']
 
+"""
+#Esto es para saber los tags disponibles a utilizar del diccionario:
+ratos_test = datos_empresa['2016'].keys()
+print(list(ratos_test))
+"""
+#4- Los mostramos en una lista ordenada
+años_disponibles = sorted(list(datos_empresa.keys()))
+print(f"\nAños historicos detectados :  {años_disponibles}")
