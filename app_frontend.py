f9794bcc7e31b1943a5ee17795e6f11e14451742
Fabien <garaoz@unsa.edu.pe>
2026-09-09 01:07:25 -0500

    Mostramos en pantalla los valores del JSON:

--- app_frontend.py
+++ app_frontend.py
@@ -25,7 +25,7 @@ print(f"\nAños historicos detectados :  {años_disponibles}")
 
 #5- Redefinimos los ratios para poder utilizarlos libremente y en el orden deseado.
 
-ratios_a_mostrar_1 = ['Utilidad Neta', 'Margen Neto', 'ROE', 'ROA', 'Prueba ácida', 'Ratio Deuda/Activo' ]
+ratios_a_mostrar_1 = ['Net Income', 'Margen Neto', 'ROE', 'ROA', 'Prueba ácida', 'Ratio Deuda/Activo' ]
 
 #6- Creamos la cabecera horizontal uniendo los años del JSON:
 columna_por_años = " | ".join([f"{año:<10}" for año in años_disponibles])
@@ -35,4 +35,24 @@ cabecera = f"{'Ratio /  ': <22} | {columna_por_años}"
 
 print("\n" + "=" *len(cabecera))
 print(cabecera)
+print("="*len(cabecera))
+print("\n")
+
+#7- Mostrar en bucle los años
+for ratio in ratios_a_mostrar_1:
+    valores_fila = []
+
+    for año in años_disponibles:
+        #Se entra al JSON:
+        valor = datos_empresa[año].get(ratio, 0)
+
+        #Formateamos el numero con el formato
+        valores_fila.append(f"{valor:<10.2f}")
+
+        #Guardamos en formato 2. decimales
+    fila_completa = " | ".join(valores_fila)
+
+        #Se imprime en consola con formato:
+    print(f"{ratio:<22} | {fila_completa}")
+
 print("="*len(cabecera))
